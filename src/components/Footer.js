2090066a8e53c2e8d70b8168ff971df65bd45d64
bonini81 <andresdominguez81@gmail.com>
2020-05-27 19:36:57 -0500

    Fixing Footer issue

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,7 @@ const Footer = () => {
     return (
       <React.Fragment>
  
-              <nav className="navbar navbar-expand-sm navbar-dark bg-dark">
+              <nav className="navbar fixed-bottom navbar-expand-sm navbar-dark bg-dark">
   <a className="navbar-brand" href="#">MyPocket Links</a>
   <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
     <span className="navbar-toggler-icon"></span>
